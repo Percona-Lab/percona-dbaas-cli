992db06d0202baf5cfa629385afa5d0fbeb61189
Andrew Pogrebnoy <absourd.noise@gmail.com>
2019-03-29 16:58:02 +0200

    Create cluster

--- dbaas/pxc/pxc.go
+++ dbaas/pxc/pxc.go
@@ -15,6 +15,10 @@
 package pxc
 
 import (
+	"bytes"
+	"encoding/base64"
+
+	"github.com/pkg/errors"
 	"github.com/spf13/pflag"
 
 	"github.com/Percona-Lab/percona-dbaas-cli/dbaas"
@@ -33,19 +37,47 @@ type PXC struct {
 }
 
 func New(name string, version Version) (*PXC, error) {
-	return &PXC{name: name}, nil
+	pxc := &PXC{
+		name:   name,
+		obj:    objects[version],
+		config: Config{ClusterName: name},
+	}
+
+	return pxc, nil
 }
 
 func (p PXC) Bundle() string {
 	return p.obj.Bundle
 }
 
-func (p PXC) Secrets() string {
-	return p.obj.Secrets
+func (p PXC) Secrets() (string, error) {
+	pass := dbaas.GenSecrets(p.obj.Secrets.Keys)
+	pb64 := make(map[string]string, len(pass)+1)
+
+	pb64["ClusterName"] = p.name
+
+	for k, v := range pass {
+		buf := make([]byte, base64.StdEncoding.EncodedLen(len(v)))
+		base64.StdEncoding.Encode(buf, v)
+		pb64[k] = string(buf)
+	}
+
+	var buf bytes.Buffer
+	err := p.obj.Secrets.Data.Execute(&buf, pb64)
+	if err != nil {
+		return "", errors.Wrap(err, "parse template:")
+	}
+
+	return buf.String(), nil
 }
 
-func (p PXC) App(config map[string]string) string {
-	return p.obj.CR
+func (p PXC) App() (string, error) {
+	var buf bytes.Buffer
+	err := p.obj.CR.Execute(&buf, p.config)
+	if err != nil {
+		return "", errors.Wrap(err, "parse template:")
+	}
+	return buf.String(), nil
 }
 
 func (p PXC) SetConfig(f *pflag.FlagSet) error {
