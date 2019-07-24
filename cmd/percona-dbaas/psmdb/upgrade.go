9e526f1db7106f72a3e722fae74af7d28ac86c8e
Ivan Pylypenko <ivan.pylypenko@percona.com>
2019-07-24 18:53:28 +0300

    CLOUD-336 Help tip text update

--- cmd/percona-dbaas/psmdb/upgrade.go
+++ cmd/percona-dbaas/psmdb/upgrade.go
@@ -32,7 +32,7 @@ import (
 // upgradeCmd represents the edit command
 var upgradeCmd = &cobra.Command{
 	Use:   "upgrade <psmdb-cluster-name> <to-version>",
-	Short: "Upgrade Percona Server MongoDB cluster",
+	Short: "Upgrade Percona Server for MongoDB",
 	Args: func(cmd *cobra.Command, args []string) error {
 		if len(args) == 0 {
 			return errors.New("You have to specify psmdb-cluster-name")
