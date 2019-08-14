a14102991a3f7918c4e3b722fcdd87e5ad7f6e83
Max Dudin <dudin.mv@gmail.com>
2019-08-14 16:27:24 +0300

    Add psmdb tests

--- integtests/datafactory/datafactory.go
+++ integtests/datafactory/datafactory.go
@@ -15,7 +15,7 @@ func GetCreatePXCInstanceData() CaseData {
 			"service_id":"percona-xtradb-cluster-id",
 			"plan_id":"percona-xtradb-id",
 			"parameters":{
-				"cluster_name":"test",
+				"cluster_name":"test-pxc",
 				"replicas":3,
 				"proxy_sql_replicas":1,
 				"topology_key": "none",
@@ -35,9 +35,77 @@ func GetGetPXCInstanceData() CaseData {
 	}
 }
 
+func GetUpdatePXCInstanceData() CaseData {
+	return CaseData{
+		Endpoint: "/v2/service_instances/test-pxc-instance",
+		ReqType:  "UPDATE",
+		ReqData: []byte(`{
+			"service_id":"percona-xtradb-cluster-id",
+			"plan_id":"percona-xtradb-id",
+			"parameters":{
+				"cluster_name":"test-pxc",
+				"replicas":2
+				}
+			}`),
+		RespData: `{"dashboard_url":"","last_operation":{"state":"in progress","description":"creating service instance...","async_poll_interval_seconds":10}}`,
+	}
+}
+
 func GetDeletePXCInstanceData() CaseData {
 	return CaseData{
 		Endpoint: "/v2/service_instances/test-pxc-instance",
+		ReqType:  "DELETE",
+		ReqData:  []byte(``),
+		RespData: "",
+	}
+}
+
+func GetCreatePSMDBInstanceData() CaseData {
+	return CaseData{
+		Endpoint: "/v2/service_instances/test-psmdb-instance",
+		ReqType:  "PUT",
+		ReqData: []byte(`{
+			"service_id":"percona-server-for-mongodb-id",
+			"plan_id":"percona-server-for-mongodb-id",
+			"parameters":{
+				"cluster_name":"test-psmdb",
+				"replicas":3,
+				"topology_key": "none",
+				"size": "1Gi"
+				}
+			}`),
+		RespData: `{"dashboard_url":"","last_operation":{"state":"in progress","description":"creating service instance...","async_poll_interval_seconds":10}}`,
+	}
+}
+
+func GetGetPSMDBInstanceData() CaseData {
+	return CaseData{
+		Endpoint: "/v2/service_instances/test-psmdb-instance",
+		ReqType:  "GET",
+		ReqData:  []byte(``),
+		RespData: `{"state":"in progress","description":"creating service instance...","async_poll_interval_seconds":10}`,
+	}
+}
+
+func GetUpdatePSMDBInstanceData() CaseData {
+	return CaseData{
+		Endpoint: "/v2/service_instances/test-psmdb-instance",
+		ReqType:  "UPDATE",
+		ReqData: []byte(`{
+			"service_id":"percona-server-for-mongodb-id",
+			"plan_id":"percona-server-for-mongodb-id",
+			"parameters":{
+				"cluster_name":"test-psmdb",
+				"replicas":2
+				}
+			}`),
+		RespData: `{"dashboard_url":"","last_operation":{"state":"in progress","description":"creating service instance...","async_poll_interval_seconds":10}}`,
+	}
+}
+
+func GetDeletePSMDBInstanceData() CaseData {
+	return CaseData{
+		Endpoint: "/v2/service_instances/test-psmdb-instance",
 		ReqType:  "DELETE",
 		ReqData:  []byte(``),
 		RespData: "{}",
