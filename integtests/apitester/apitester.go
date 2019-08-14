3541528afa1a8622e877ab3cf9e826e0f0676e04
Max Dudin <dudin.mv@gmail.com>
2019-08-14 16:27:24 +0300

    Add psmdb tests

--- integtests/apitester/apitester.go
+++ integtests/apitester/apitester.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/Percona-Lab/percona-dbaas-cli/integtests/datafactory"
 )
@@ -35,11 +36,12 @@ func New(addres string) Config {
 // Run starts testing
 func (c *Config) Run() error {
 	for _, testCase := range c.Cases {
-		log.Println(testCase.Endpoint)
+		log.Println(testCase.Endpoint, testCase.ReqType)
 		err := c.check(testCase)
 		if err != nil {
 			return err
 		}
+		time.Sleep(5 * time.Second)
 	}
 
 	return nil
@@ -54,8 +56,8 @@ func (c *Config) check(testCase Case) error {
 		log.Println(respStatus)
 		//return fmt.Errorf("Wrong status")
 	}
-	log.Println(string(respData))
-	if string(respData) != testCase.RespData {
+	log.Println(respStatus, string(respData))
+	if len(testCase.ReqData) > 0 && string(respData) != testCase.RespData {
 		return fmt.Errorf("Wrong responce")
 	}
 	return nil
@@ -81,14 +83,13 @@ func (c *Config) Request(endpoint, reqType string, reqBody []byte) ([]byte, int,
 }
 
 func (t *Config) setCases() {
-	//var cases []Case
-	/*createPXCInstanceData := datafactory.GetCreatePXCInstanceData()
+	createPXCInstanceData := datafactory.GetCreatePXCInstanceData()
 	t.Cases = append(t.Cases, Case{
 		Endpoint: createPXCInstanceData.Endpoint,
 		ReqType:  createPXCInstanceData.ReqType,
 		ReqData:  createPXCInstanceData.ReqData,
 		RespData: createPXCInstanceData.RespData,
-	})*/
+	})
 	getPXCInstanceData := datafactory.GetGetPXCInstanceData()
 	t.Cases = append(t.Cases, Case{
 		Endpoint: getPXCInstanceData.Endpoint,
@@ -96,11 +97,46 @@ func (t *Config) setCases() {
 		ReqData:  getPXCInstanceData.ReqData,
 		RespData: getPXCInstanceData.RespData,
 	})
+	updatePXCInstanceData := datafactory.GetGetPXCInstanceData()
+	t.Cases = append(t.Cases, Case{
+		Endpoint: updatePXCInstanceData.Endpoint,
+		ReqType:  updatePXCInstanceData.ReqType,
+		ReqData:  updatePXCInstanceData.ReqData,
+		RespData: updatePXCInstanceData.RespData,
+	})
 	deletePXCInstanceData := datafactory.GetDeletePXCInstanceData()
 	t.Cases = append(t.Cases, Case{
 		Endpoint: deletePXCInstanceData.Endpoint,
 		ReqType:  deletePXCInstanceData.ReqType,
 		ReqData:  deletePXCInstanceData.ReqData,
 		RespData: deletePXCInstanceData.RespData,
+	})
+	createPSMDBInstanceData := datafactory.GetCreatePSMDBInstanceData()
+	t.Cases = append(t.Cases, Case{
+		Endpoint: createPSMDBInstanceData.Endpoint,
+		ReqType:  createPSMDBInstanceData.ReqType,
+		ReqData:  createPSMDBInstanceData.ReqData,
+		RespData: createPSMDBInstanceData.RespData,
+	})
+	getPSMDBInstanceData := datafactory.GetGetPSMDBInstanceData()
+	t.Cases = append(t.Cases, Case{
+		Endpoint: getPSMDBInstanceData.Endpoint,
+		ReqType:  getPSMDBInstanceData.ReqType,
+		ReqData:  getPSMDBInstanceData.ReqData,
+		RespData: getPSMDBInstanceData.RespData,
+	})
+	updatePSMDBInstanceData := datafactory.GetGetPSMDBInstanceData()
+	t.Cases = append(t.Cases, Case{
+		Endpoint: updatePSMDBInstanceData.Endpoint,
+		ReqType:  updatePSMDBInstanceData.ReqType,
+		ReqData:  updatePSMDBInstanceData.ReqData,
+		RespData: updatePSMDBInstanceData.RespData,
+	})
+	deletePSMDBInstanceData := datafactory.GetDeletePSMDBInstanceData()
+	t.Cases = append(t.Cases, Case{
+		Endpoint: deletePSMDBInstanceData.Endpoint,
+		ReqType:  deletePSMDBInstanceData.ReqType,
+		ReqData:  deletePSMDBInstanceData.ReqData,
+		RespData: deletePSMDBInstanceData.RespData,
 	})
 }
