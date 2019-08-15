5e3400e757448b8f1fb5f19c462816d8f9d79557
Max Dudin <dudin.mv@gmail.com>
2019-08-15 13:12:59 +0300

    Update unit tests

--- broker/controller_test.go
+++ broker/controller_test.go
@@ -1,28 +1,60 @@
 package broker
 
-import "testing"
+import (
+	"log"
+	"testing"
+)
 
 func TestUpdateInstanceMapTests(t *testing.T) {
 	var c Controller
 	c.instanceMap = make(map[string]*ServiceInstance)
-	s := []byte(`'{"id":"","dashboard_url":"","service_id":"percona-xtradb-cluster-id","plan_id":"percona-xtradb-id","organization_guid":"","space_guid":"","parameters":{"cluster_name":"test","replicas":3,"proxy_sql_replicas":1,"topology_key":"none","size":"1Gi"},"context":{}}'`)
-	err := c.updateInstanceMap(s)
-	if err != nil {
-		t.Error("one instance:", err)
+	type test struct {
+		instQuant    int
+		clusterNames map[string]string
+		data         []byte
 	}
-	s = []byte(`'{"id":"","dashboard_url":"","service_id":"percona-xtradb-cluster-id","plan_id":"percona-xtradb-id","organization_guid":"","space_guid":"","parameters":{"cluster_name":"test","replicas":3,"proxy_sql_replicas":1,"topology_key":"none","size":"1Gi"},"context":{}} {"id":"","dashboard_url":"","service_id":"percona-xtradb-cluster-id","plan_id":"percona-xtradb-id","organization_guid":"","space_guid":"","parameters":{"cluster_name":"test","replicas":3,"proxy_sql_replicas":1,"topology_key":"none","size":"1Gi"},"context":{}}'`)
-	err = c.updateInstanceMap(s)
-	if err != nil {
-		t.Error("two instance:", err)
+	tests := map[string]test{
+		"one instance": {
+			instQuant: 1,
+			clusterNames: map[string]string{
+				"pxc-1": "test",
+			},
+			data: []byte(`'{"id":"pxc-1","dashboard_url":"","service_id":"percona-xtradb-cluster-id","plan_id":"percona-xtradb-id","organization_guid":"","space_guid":"","parameters":{"cluster_name":"test","replicas":3,"proxy_sql_replicas":1,"topology_key":"none","size":"1Gi"},"context":{}}'`),
+		},
+		"two instances": {
+			instQuant: 2,
+			clusterNames: map[string]string{
+				"pxc-1":   "test",
+				"psmdb-1": "psmdb",
+			},
+			data: []byte(`'{"id":"pxc-1","dashboard_url":"","service_id":"percona-xtradb-cluster-id","plan_id":"percona-xtradb-id","organization_guid":"","space_guid":"","parameters":{"cluster_name":"test","replicas":3,"proxy_sql_replicas":1,"topology_key":"none","size":"1Gi"},"context":{}} {"id":"psmdb-1","dashboard_url":"","service_id":"percona-server-for-mongodb-id","plan_id":"percona-server-for-mongodb-id","organization_guid":"","space_guid":"","parameters":{"cluster_name":"psmdb","replicas":5,"proxy_sql_replicas":1,"topology_key":"none","size":"2Gi"},"context":{}}'`),
+		},
+		"three instances": {
+			instQuant: 3,
+			clusterNames: map[string]string{
+				"pxc-1":   "test",
+				"psmdb-1": "psmdb",
+				"pxc-2":   "pxc-2",
+			},
+			data: []byte(`'{"id":"pxc-1","dashboard_url":"","service_id":"percona-xtradb-cluster-id","plan_id":"percona-xtradb-id","organization_guid":"","space_guid":"","parameters":{"cluster_name":"test","replicas":3,"proxy_sql_replicas":1,"topology_key":"none","size":"1Gi"},"context":{}} {"id":"psmdb-1","dashboard_url":"","service_id":"percona-server-for-mongodb-id","plan_id":"percona-server-for-mongodb-id","organization_guid":"","space_guid":"","parameters":{"cluster_name":"psmdb","replicas":5,"proxy_sql_replicas":1,"topology_key":"none","size":"2Gi"},"context":{}} {"id":"pxc-2","dashboard_url":"","service_id":"percona-xtradb-cluster-id","plan_id":"percona-xtradb-id","organization_guid":"","space_guid":"","parameters":{"cluster_name":"pxc-2","replicas":5,"proxy_sql_replicas":1,"topology_key":"none","size":"3Gi"},"context":{}}'`),
+		},
 	}
-	s = []byte(`'{"id":"","dashboard_url":"","service_id":"percona-xtradb-cluster-id","plan_id":"percona-xtradb-id","organization_guid":"","space_guid":"","parameters":{"cluster_name":"test","replicas":3,"proxy_sql_replicas":1,"topology_key":"none","size":"1Gi"},"context":{}} {"id":"","dashboard_url":"","service_id":"percona-xtradb-cluster-id","plan_id":"percona-xtradb-id","organization_guid":"","space_guid":"","parameters":{"cluster_name":"test","replicas":3,"proxy_sql_replicas":1,"topology_key":"none","size":"1Gi"},"context":{}} {"id":"","dashboard_url":"","service_id":"percona-xtradb-cluster-id","plan_id":"percona-xtradb-id","organization_guid":"","space_guid":"","parameters":{"cluster_name":"test","replicas":3,"proxy_sql_replicas":1,"topology_key":"none","size":"1Gi"},"context":{}}'`)
-	err = c.updateInstanceMap(s)
-	if err != nil {
-		t.Error("three instance:", err)
-	}
-	s = []byte(`'{}}'`)
-	err = c.updateInstanceMap(s)
-	if err == nil {
-		t.Error("invalid JSON:", err)
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			err := c.updateInstanceMap(test.data)
+			if err != nil {
+				t.Error(name, err)
+			}
+			log.Println(len(c.instanceMap))
+			if len(c.instanceMap) != test.instQuant {
+				t.Error(name+":", "Wrong instance quantoty")
+			}
+			for id, clusterName := range test.clusterNames {
+				if clusterName != c.instanceMap[id].Parameters.ClusterName {
+					t.Error(name+":", "Wrong instance data")
+				}
+			}
+		})
 	}
 }
