cc8de523331e5de8e899373fa94fee3b44dd54ab
Andrew Pogrebnoy <absourd.noise@gmail.com>
2019-12-13 12:03:46 +0200

    Options parsing refactoring

--- dbaas-lib/engines/k8s-pxc/options.go
+++ dbaas-lib/engines/k8s-pxc/options.go
@@ -2,18 +2,14 @@ package pxc
 
 import (
 	"reflect"
-	"strconv"
-	"strings"
 
 	"github.com/Percona-Lab/percona-dbaas-cli/dbaas-lib/engines/k8s-pxc/types/config"
-	"github.com/pkg/errors"
+	"github.com/Percona-Lab/percona-dbaas-cli/dbaas-lib/options"
 )
 
-var currectOptions map[string]string
-
 // ParseOptions parse PXC options given in "object.paramValue=val,objectTwo.paramValue=val" string
-func (p *PXC) ParseOptions(options string) error {
-	options = strings.ToLower(options)
+func (p *PXC) ParseOptions(opts string) error {
+
 	var c config.ClusterConfig
 
 	res := config.PodResources{
@@ -35,183 +31,11 @@ func (p *PXC) ParseOptions(options string) error {
 	c.ProxySQL.Affinity = aff
 	c.S3.SkipStorage = true
 
-	if len(options) != 0 {
-		currectOptions = make(map[string]string)
-		keys(reflect.TypeOf(config.ClusterConfig{}), "", "")
-
-		optArr := strings.Split(options, ",")
-
-		for _, str := range optArr {
-			v := strings.Split(str, "=")
-			if _, ok := currectOptions[v[0]]; !ok {
-				return errors.New("incorrect options")
-			}
-			if len(v) > 1 {
-				err := set(&c, currectOptions[v[0]], v[1])
-				if err != nil {
-					return errors.Wrap(err, "set value")
-				}
-			}
-		}
+	err := options.Parse(&c, reflect.TypeOf(c), opts)
+	if err != nil {
+		return err
 	}
 	p.config = c
 
 	return nil
-}
-
-func set(i *config.ClusterConfig, field string, value string) error {
-	fs := strings.Split(field, ".")
-	v := reflect.ValueOf(i).Elem()
-
-	switch len(fs) {
-	case 1:
-		val, err := getValue(value, v.FieldByName(fs[0]))
-		if err != nil {
-			return err
-		}
-		v.FieldByName(fs[0]).Set(val)
-	case 2:
-		val, err := getValue(value, v.FieldByName(fs[0]).FieldByName(fs[1]))
-		if err != nil {
-			return err
-		}
-		v.FieldByName(fs[0]).FieldByName(fs[1]).Set(val)
-	case 3:
-		val, err := getValue(value, v.FieldByName(fs[0]).FieldByName(fs[1]).FieldByName(fs[2]))
-		if err != nil {
-			return err
-		}
-		v.FieldByName(fs[0]).FieldByName(fs[1]).FieldByName(fs[2]).Set(val)
-	case 4:
-		val, err := getValue(value, v.FieldByName(fs[0]).FieldByName(fs[1]).FieldByName(fs[2]).FieldByName(fs[3]))
-		if err != nil {
-			return err
-		}
-		v.FieldByName(fs[0]).FieldByName(fs[1]).FieldByName(fs[2]).FieldByName(fs[3]).Set(val)
-	case 5:
-		val, err := getValue(value, v.FieldByName(fs[0]).FieldByName(fs[1]).FieldByName(fs[2]).FieldByName(fs[3]).FieldByName(fs[4]))
-		if err != nil {
-			return err
-		}
-		v.FieldByName(fs[0]).FieldByName(fs[1]).FieldByName(fs[2]).FieldByName(fs[3]).FieldByName(fs[4]).Set(val)
-	}
-	return nil
-}
-
-func getValue(value string, field reflect.Value) (reflect.Value, error) {
-	var fieldValue interface{}
-	var pointer bool
-	if strings.Contains(field.Type().String(), "*") {
-		pointer = true
-	}
-	fieldType := strings.Trim(field.Type().String(), "*")
-	switch fieldType {
-	case "int":
-		v, err := strconv.Atoi(value)
-		if err != nil {
-			return reflect.Value{}, err
-		}
-		fieldValue = v
-		if pointer {
-			fieldValue = &v
-		}
-	case "int32":
-		v, err := strconv.Atoi(value)
-		if err != nil {
-			return reflect.Value{}, err
-		}
-		i32v := int32(v)
-		fieldValue = i32v
-		if pointer {
-			fieldValue = &i32v
-		}
-	case "int64":
-		v, err := strconv.ParseInt(value, 10, 64)
-		if err != nil {
-			return reflect.Value{}, err
-		}
-		fieldValue = v
-		if pointer {
-			fieldValue = &v
-		}
-	case "float32":
-		v, err := strconv.ParseFloat(value, 32)
-		if err != nil {
-			return reflect.Value{}, err
-		}
-		f32v := float32(v)
-		fieldValue = f32v
-		if pointer {
-			fieldValue = &f32v
-		}
-	case "float64":
-		v, err := strconv.ParseFloat(value, 64)
-		if err != nil {
-			return reflect.Value{}, err
-		}
-		fieldValue = v
-		if pointer {
-			fieldValue = &v
-		}
-	case "bool":
-		v, err := strconv.ParseBool(value)
-		if err != nil {
-			return reflect.Value{}, err
-		}
-		fieldValue = v
-		if pointer {
-			fieldValue = &v
-		}
-	case "map[string]string":
-		v, err := parseMapValue(value)
-		if err != nil {
-			return reflect.Value{}, err
-		}
-		fieldValue = v
-		if pointer {
-			fieldValue = &v
-		}
-		return reflect.Indirect(reflect.ValueOf(v)), nil
-	case "string":
-		fieldValue = value
-		if pointer {
-			fieldValue = &value
-		}
-	}
-
-	return reflect.ValueOf(fieldValue), nil
-}
-
-func parseMapValue(s string) (map[string]string, error) {
-	value := make(map[string]string)
-	sSlice := strings.Split(s, ";")
-	if len(sSlice) == 0 {
-		return nil, errors.New("empty value")
-	}
-	for _, v := range sSlice {
-		vSlice := strings.Split(v, ":")
-		if len(vSlice) != 2 {
-			return nil, errors.New("empty map value")
-		}
-		value[vSlice[0]] = vSlice[1]
-	}
-
-	return value, nil
-}
-
-func keys(t reflect.Type, prevType, prevName string) map[string]string {
-	var v = make(map[string]string)
-	for i := 0; i < t.NumField(); i++ {
-		name := strings.TrimSpace(strings.Split(t.Field(i).Tag.Get("json"), ",")[0])
-		if t.Field(i).Type.Kind() == reflect.Struct {
-			for name, nType := range keys(t.Field(i).Type, prevType+t.Field(i).Name+".", prevName+name+".") {
-				currectOptions[strings.ToLower(prevName+name+"."+name)] = prevType + t.Field(i).Name + "." + nType
-			}
-		} else {
-			v[strings.ToLower(name)] = t.Field(i).Name
-			currectOptions[strings.ToLower(prevName+name)] = prevType + t.Field(i).Name
-		}
-	}
-
-	return v
 }
---
+++ dbaas-lib/options/options.go
@@ -0,0 +1,167 @@
+package options
+
+import (
+	"reflect"
+	"strconv"
+	"strings"
+
+	"github.com/pkg/errors"
+)
+
+// Parse parses options from the given string in format "object.paramValue=val,objectTwo.paramValue=val"
+// and assigned it into the given "to" of type type
+func Parse(to interface{}, typ reflect.Type, options string) error {
+	if options == "" {
+		return nil
+	}
+
+	opts := make(map[string]string)
+	validConfKeys(typ, opts, "", "")
+
+	optArr := strings.Split(options, ",")
+	for _, str := range optArr {
+		v := strings.Split(str, "=")
+		if _, ok := opts[v[0]]; !ok {
+			return errors.Errorf("invalid option %s", v[0])
+		}
+		if len(v) > 1 {
+			fs := strings.Split(opts[v[0]], ".")
+			rv := reflect.ValueOf(to).Elem()
+			for _, f := range fs {
+				rv = rv.FieldByName(f)
+			}
+			err := setValue(rv, v[1])
+			if err != nil {
+				return errors.Wrapf(err, "set value %s=%s", v[0], v[1])
+			}
+		}
+	}
+
+	return nil
+}
+
+func setValue(val reflect.Value, value string) error {
+
+	if val.Kind() == reflect.Ptr {
+		if val.IsZero() {
+			val.Set(reflect.New(val.Type().Elem()))
+		}
+		val = reflect.Indirect(val)
+	}
+
+	switch val.Kind() {
+	default:
+		// TODO: maps, slices
+		return errors.Errorf("type %v not implemented", val.Kind())
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		v, err := strconv.ParseInt(value, 10, 64)
+		if err != nil || val.OverflowInt(v) {
+			return errors.Errorf("parse value %s: %v", val, err)
+		}
+		val.SetInt(v)
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		v, err := strconv.ParseUint(value, 10, 64)
+		if err != nil || val.OverflowUint(v) {
+			return errors.Errorf("parse value %s: %v", val, err)
+		}
+		val.SetUint(v)
+	case reflect.Float32, reflect.Float64:
+		v, err := strconv.ParseFloat(value, 64)
+		if err != nil || val.OverflowFloat(v) {
+			return errors.Errorf("parse value %s: %v", val, err)
+		}
+		val.SetFloat(v)
+	case reflect.Bool:
+		v, err := strconv.ParseBool(value)
+		if err != nil {
+			return errors.Errorf("parse value %s: %v", val, err)
+		}
+		val.SetBool(v)
+	case reflect.String:
+		val.SetString(value)
+
+	}
+	return nil
+}
+
+// TODO: maps, slices
+func getValue(val reflect.Value, value string) (reflect.Value, error) {
+	if val.Kind() == reflect.Ptr {
+		iv, err := getValue(val.Elem(), value)
+		if err != nil {
+			return reflect.Value{}, err
+		}
+		reflect.Indirect(val).Set(iv)
+		return val, nil
+	}
+
+	rv := reflect.Value{}
+	switch val.Kind() {
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		v, err := strconv.ParseInt(value, 10, 64)
+		if err != nil || val.OverflowInt(v) {
+			return rv, errors.Errorf("parse value %s: %v", val, err)
+		}
+		rv = reflect.ValueOf(v).Convert(val.Type())
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		v, err := strconv.ParseUint(value, 10, 64)
+		if err != nil || val.OverflowUint(v) {
+			return rv, errors.Errorf("parse value %s: %v", val, err)
+		}
+		rv = reflect.ValueOf(v).Convert(val.Type())
+	case reflect.Float32, reflect.Float64:
+		v, err := strconv.ParseFloat(value, 64)
+		if err != nil || val.OverflowFloat(v) {
+			return rv, errors.Errorf("parse value %s: %v", val, err)
+		}
+		rv = reflect.ValueOf(v).Convert(val.Type())
+	case reflect.Bool:
+		v, err := strconv.ParseBool(value)
+		if err != nil {
+			return rv, errors.Errorf("parse value %s: %v", val, err)
+		}
+		rv = reflect.ValueOf(v)
+	case reflect.String:
+		rv = reflect.ValueOf(value)
+	}
+
+	return rv, nil
+}
+
+func parseMapValue(s string) (map[string]string, error) {
+	value := make(map[string]string)
+	sSlice := strings.Split(s, ";")
+	if len(sSlice) == 0 {
+		return nil, errors.New("empty value")
+	}
+	for _, v := range sSlice {
+		vSlice := strings.Split(v, ":")
+		if len(vSlice) != 2 {
+			return nil, errors.New("empty map value")
+		}
+		value[vSlice[0]] = vSlice[1]
+	}
+
+	return value, nil
+}
+
+func validConfKeys(t reflect.Type, to map[string]string, pk, pv string) {
+	for i := 0; i < t.NumField(); i++ {
+		name := strings.TrimSpace(strings.Split(t.Field(i).Tag.Get("json"), ",")[0])
+		if name == "" {
+			name = t.Field(i).Name
+		}
+		if pk != "" {
+			name = pk + "." + name
+		}
+		kt := t.Field(i).Name
+		if pv != "" {
+			kt = pv + "." + kt
+		}
+		if t.Field(i).Type.Kind() == reflect.Struct {
+			validConfKeys(t.Field(i).Type, to, name, kt)
+		} else {
+			to[name] = kt
+		}
+	}
+}
