3fab1257538437b3781577a5f6c6d1a487782867
Max Dudin <dudin.mv@gmail.com>
2019-12-17 22:38:21 +0200

    CLOUD-487 remove comments

--- dbaas-lib/options/options.go
+++ dbaas-lib/options/options.go
@@ -95,69 +95,6 @@ func setValue(val reflect.Value, value string) error {
 	return nil
 }
 
-/*
-// TODO: maps, slices
-func getValue(val reflect.Value, value string) (reflect.Value, error) {
-	var ival reflect.Value
-	if val.Kind() == reflect.Ptr {
-
-		//iv, err := getValue(val.Elem(), value)
-		//val = reflect.Indirect(val)
-
-		log.Println("t kind:", val.Addr().Type())
-		newVal := reflect.Indirect(reflect.New(val.Addr().Type())).Elem()
-		iv, err := getValue(newVal, value)
-		if err != nil {
-			return reflect.Value{}, err
-		}
-		ival = iv
-
-		//reflect.Indirect(val).Set(iv)
-		//return val, nil
-		return ival, nil
-	}
-	log.Println("kind:", val.Kind())
-	log.Println("type:", val.Type())
-
-	rv := reflect.Value{}
-	switch val.Kind() {
-	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-		v, err := strconv.ParseInt(value, 10, 64)
-		if err != nil || val.OverflowInt(v) {
-			return rv, errors.Errorf("parse value %s: %v", val, err)
-		}
-		rv = reflect.ValueOf(v).Convert(val.Type())
-	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
-		v, err := strconv.ParseUint(value, 10, 64)
-		if err != nil || val.OverflowUint(v) {
-			return rv, errors.Errorf("parse value %s: %v", val, err)
-		}
-		rv = reflect.ValueOf(v).Convert(val.Type())
-	case reflect.Float32, reflect.Float64:
-		v, err := strconv.ParseFloat(value, 64)
-		if err != nil || val.OverflowFloat(v) {
-			return rv, errors.Errorf("parse value %s: %v", val, err)
-		}
-		rv = reflect.ValueOf(v).Convert(val.Type())
-	case reflect.Bool:
-		v, err := strconv.ParseBool(value)
-		if err != nil {
-			return rv, errors.Errorf("parse value %s: %v", val, err)
-		}
-		rv = reflect.ValueOf(v)
-	case reflect.String:
-		rv = reflect.ValueOf(value)
-	case reflect.Map:
-		//v, err := parseMapValue(value)
-		//if err != nil {
-		//	return rv, errors.Errorf("parse value %s: %v", val, err)
-		//}
-		//rv = reflect.ValueOf(v)
-	}
-
-	return rv, nil
-}
-*/
 func parseMapValue(s string, refValue reflect.Value) (reflect.Value, error) {
 	value := reflect.MakeMap(refValue.Type())
 
