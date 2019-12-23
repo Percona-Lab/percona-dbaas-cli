dd75bf2ea5eb491b32b45e3e161bfdcd124d0ad4
Max Dudin <dudin.mv@gmail.com>
2019-12-23 14:55:59 +0200

    CLOUD-490 Add mongo to cli

---
+++ dbaas-cli/cmd/mongo/create.go
@@ -0,0 +1,97 @@
+// Copyright © 2019 Percona, LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package mongo
+
+import (
+	"strings"
+	"time"
+
+	"github.com/pkg/errors"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+
+	dbaas "github.com/Percona-Lab/percona-dbaas-cli/dbaas-lib"
+	_ "github.com/Percona-Lab/percona-dbaas-cli/dbaas-lib/engines/k8s-psmdb"
+)
+
+const (
+	defaultVersion = "default"
+	maxTries       = 1200
+)
+
+// createCmd represents the create command
+var createCmd = &cobra.Command{
+	Use:   "create-db <mongo-cluster-name>",
+	Short: "Create MongoDB cluster",
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) == 0 {
+			return errors.New("You have to specify resource name")
+		}
+
+		return nil
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		instance := dbaas.Instance{
+			Name:          args[0],
+			EngineOptions: *options,
+			Engine:        *engine,
+			Provider:      *provider,
+		}
+
+		dotPrinter.Start("Starting")
+		err := dbaas.CreateDB(instance)
+		if err != nil {
+			dotPrinter.Stop("error")
+			log.Error("create db: ", err)
+			return
+		}
+		tries := 0
+		tckr := time.NewTicker(500 * time.Millisecond)
+		defer tckr.Stop()
+		for range tckr.C {
+			cluster, err := dbaas.DescribeDB(instance)
+			if err != nil {
+				//dotPrinter.StopPrintDot("error")
+				//log.Error("check db: ", err)
+				continue
+			}
+			if cluster.Status == "ready" {
+				dotPrinter.Stop("done")
+				log.Println("Database started successfully, connection details are below:")
+				cluster.Message = strings.Replace(cluster.Message, "PASSWORD", cluster.Pass, 1)
+				log.Println(cluster)
+				return
+			}
+			if tries >= maxTries {
+				dotPrinter.Stop("error")
+				log.Error("unable to start cluster. cluster status: ", cluster.Status)
+				return
+			}
+			tries++
+		}
+	},
+}
+
+var options *string
+var provider *string
+var engine *string
+
+func init() {
+	options = createCmd.Flags().String("options", "", "Engine options")
+	provider = createCmd.Flags().String("provider", "k8s", "Provider")
+	engine = createCmd.Flags().String("engine", "psmdb", "Engine")
+
+	MongoCmd.AddCommand(createCmd)
+}
---
+++ dbaas-cli/cmd/mongo/describe.go
@@ -0,0 +1,83 @@
+// Copyright © 2019 Percona, LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package mongo
+
+import (
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+
+	dbaas "github.com/Percona-Lab/percona-dbaas-cli/dbaas-lib"
+)
+
+// describeCmd represents the describe command
+var describeCmd = &cobra.Command{
+	Use:   "describe-db <mongo-cluster-name>",
+	Short: "Describe MongoDB cluster or list clusters",
+	Run: func(cmd *cobra.Command, args []string) {
+		name := ""
+		if len(args) > 0 {
+			name = args[0]
+		}
+		instance := dbaas.Instance{
+			Name:          name,
+			EngineOptions: *options,
+			Engine:        *descrEngine,
+			Provider:      *descrProvider,
+		}
+
+		if len(name) > 0 {
+			db, err := dbaas.DescribeDB(instance)
+			if err != nil {
+				log.Error("describe db: ", err)
+				return
+			}
+			db.Pass = ""
+			log.Print(db)
+			return
+		}
+
+		listDB, err := dbaas.ListDB(instance)
+		if err != nil {
+			log.Error("list db: ", err)
+			return
+		}
+		if len(listDB) == 0 {
+			log.Println("Nothing to show")
+			return
+		}
+		log.Println("NAME                STATUS")
+		for _, db := range listDB {
+			space := "          "
+			if len(db.ResourceName) <= 10 {
+				for i := 0; i < 10-len(db.ResourceName); i++ {
+					space = space + " "
+				}
+			} else {
+				db.ResourceName = db.ResourceName[:10]
+			}
+			log.Println(db.ResourceName + space + db.Status)
+		}
+	},
+}
+
+var descrProvider *string
+var descrEngine *string
+
+func init() {
+	descrProvider = describeCmd.Flags().String("provider", "k8s", "Provider")
+	descrEngine = describeCmd.Flags().String("engine", "psmdb", "Engine")
+
+	MongoCmd.AddCommand(describeCmd)
+}
